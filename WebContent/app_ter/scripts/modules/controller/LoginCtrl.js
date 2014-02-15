6dd366062ef1a5e8a36807755ef70125bfba7e5d
rider.carrion@gmail.com <rider.carrion@gmail.com>
2014-02-15 03:20:40 +0100

    Delete old project version

--- WebContent/app_ter/scripts/modules/controller/LoginCtrl.js
+++
@@ -1,8 +0,0 @@
-(function(){
-	'use strict';
-	var module = angular.module('mod.controller');
-	module.controller('LoginCtrl', ['$scope', '$location', function($scope, $location){
-		console.log('login');
-		$scope.log = 'nihao';
-	}]);
-})();
--- WebContent/app_ter/scripts/modules/controller/UploadCtrl.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	'use strict';
-	var module = angular.module('mod.controller');
-
-	module.controller('UploadCtrl', ['$scope', '$location',
-	                                  function($scope, $location){
-
-	}]);
-})();
